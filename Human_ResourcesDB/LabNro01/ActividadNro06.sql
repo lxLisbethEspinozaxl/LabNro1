2344c2cb2656c6bff83acc64760db27fcf371e08
lxLisbethEspinozaxl <lxlisyxl@gmail.com>
2018-10-03 10:33:26 -0500

    Prueba1

--- Human_ResourcesDB/LabNro01/ActividadNro06.sql
+++ Human_ResourcesDB/LabNro01/ActividadNro06.sql
@@ -19,4 +19,19 @@ go
 select e.last_name, e.hire_date, DateName(WEEKDAY, jh.START_DATE)as 'Dia'
 from employees as e inner join job_history as jh on
 e.employee_id=jh.employee_id
-go
+GO
+
+
+/* Ejercicio 04*/
+select last_name as 'Apellidos', 'Comision'='Sin Comision' from dbo.employees where 
+commission_pct != 0 UNION select last_name as 'Apellidos', 'Comision'= Cast((salary * commission_pct) 
+as varchar(20)) from dbo.employees where commission_pct >0
+
+
+
+/* Ejercicio 06*/
+select e.last_name as 'Apellidos', j.job_title, case when j.job_id = 'AD PRES' THEN 'A' 
+when j.job_id = 'ST_MAN' THEN 'B' when j.job_id = 'IT_PROG' THEN 'C' when j.job_id = 'SA REP' 
+THEN 'D' else '0' END as 'Grados' from dbo.employees as e inner join dbo.jobs 
+as j on e.job_id=j.job_id 
+
