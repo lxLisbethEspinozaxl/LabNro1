25bf2caefd8016f72abe1f763c614bcdb9d9d73d
lxLisbethEspinozaxl <lxlisyxl@gmail.com>
2018-10-03 10:37:40 -0500

    Version1

--- Human_ResourcesDB/LabNro01/ActividadNro06.sql
+++ Human_ResourcesDB/LabNro01/ActividadNro06.sql
@@ -11,7 +11,7 @@ Plantilla de script posterior a la implementación
 */
 go
 select 'Sueldos Soñados'=(last_name + 'gana' + Cast(salary as varchar(18)) + 'pero quisiera' + Cast((salary * 3) as varchar(18)))
-from dbo.employees
+from employees
 go
 select last_name, hire_date as Revision from employees
 where hire_date between '2003-06-17' and '2005-09-21';
@@ -19,19 +19,4 @@ go
 select e.last_name, e.hire_date, DateName(WEEKDAY, jh.START_DATE)as 'Dia'
 from employees as e inner join job_history as jh on
 e.employee_id=jh.employee_id
-GO
-
-
-/* Ejercicio 04*/
-select last_name as 'Apellidos', 'Comision'='Sin Comision' from dbo.employees where 
-commission_pct != 0 UNION select last_name as 'Apellidos', 'Comision'= Cast((salary * commission_pct) 
-as varchar(20)) from dbo.employees where commission_pct >0
-
-
-
-/* Ejercicio 06*/
-select e.last_name as 'Apellidos', j.job_title, case when j.job_id = 'AD PRES' THEN 'A' 
-when j.job_id = 'ST_MAN' THEN 'B' when j.job_id = 'IT_PROG' THEN 'C' when j.job_id = 'SA REP' 
-THEN 'D' else '0' END as 'Grados' from dbo.employees as e inner join dbo.jobs 
-as j on e.job_id=j.job_id 
-
+go
